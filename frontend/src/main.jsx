89c1207ded57649b5b600f11155b3943674b96fe
Nikita Kasyanov <casyanov.nickita@gmail.com>
2024-06-01 01:43:58 +0300

    Cool project

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,15 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import App from "./App.jsx";
-// import "./index.css";
-// import "../index.html";
-// import { BrowserRouter } from "react-router-dom";
-// import StoreContextProvider from "./context/StoreContext.jsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import "../index.html";
+import { BrowserRouter } from "react-router-dom";
+import StoreContextProvider from "./context/StoreContext.jsx";
 
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <BrowserRouter>
-//     <StoreContextProvider>
-//       <App />
-//     </StoreContextProvider>
-//   </BrowserRouter>
-// );
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <StoreContextProvider>
+      <App />
+    </StoreContextProvider>
+  </BrowserRouter>
+);
